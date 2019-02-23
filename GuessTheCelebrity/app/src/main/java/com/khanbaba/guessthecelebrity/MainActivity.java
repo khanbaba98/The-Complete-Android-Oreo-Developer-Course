3cd43c90287dba5b417cde232ff3dcbd70424f4b
Rahil <47527462+khanbaba98@users.noreply.github.com>
2019-02-23 13:52:19 +0530

    added important features
    added game ending conditons
    buttons for score, result
    reset button functionality

--- GuessTheCelebrity/app/src/main/java/com/khanbaba/guessthecelebrity/MainActivity.java
+++ GuessTheCelebrity/app/src/main/java/com/khanbaba/guessthecelebrity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.khanbaba.guessthecelebrity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
+import android.widget.TextView;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,14 +22,40 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> names = new ArrayList<>();
+    ArrayList<Integer> previouslyDone = new ArrayList<>();
     ArrayList<String> imgURLs = new ArrayList<>();
     DownloadTask htmlTask = new DownloadTask();
-    ImageDownloader imgTask = new ImageDownloader();
+    TextView scoreTextView, resultTextView;
     ImageView celebImageView;
     Random rand = new Random();
-    Button button;
-    int answerPosition;
+    Button button, resetButton;
+    int answerPosition, correctAnswerCount = 0;
     String answer;
+    boolean gameFinished = false;
+
+    public void updateScore (){
+        String score = "Score : "+ String.valueOf(correctAnswerCount) + "/" + String.valueOf(names.size());
+        scoreTextView.setText((score));
+    }
+
+    public void resetGame(View view){
+        gameFinished = false;
+        previouslyDone.clear();
+        correctAnswerCount = 0;
+        setCelebrity();
+        updateScore();
+        resetButton.setVisibility(View.INVISIBLE);
+        resultTextView.setText("");
+        buttonEnablerDisabler();
+    }
+
+    public void buttonEnablerDisabler() {
+        for(int i = 0; i<=3 ; i++) {
+            int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+            button = findViewById(id);
+            button.setEnabled(!gameFinished);
+        }
+    }
 
     public int generateRandomNumber (int limit){
         int value;
@@ -38,70 +63,84 @@ public class MainActivity extends AppCompatActivity {
         return value;
     }
 
+    //Checks if button pressed has the correct answer by comparing it with button tags
     public void checkAnswer (View view){
-        Log.i("TAG","checkAnswer");
+        Log.i("TAG", "checkAnswer");
         String message = "";
-        Button button = (Button) view;
-        Log.i("button tag",view.getTag().toString());
-        if(Integer.parseInt((String) view.getTag()) == answerPosition){
+        Log.i("button tag", view.getTag().toString());
+        if (Integer.parseInt((String) view.getTag()) == answerPosition) {
             message = "Correct!";
+            resultTextView.setText(message);
+            resultTextView.setTextColor(Color.GREEN);
+            correctAnswerCount++;
         } else {
-            message = "Wrong! Answer is: "+answer;
+            message = "Answer was: " + answer;
+            resultTextView.setText(message);
+            resultTextView.setTextColor(Color.RED);
         }
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        //htmlTask.cancel(true);
-        //imgTask.cancel(true);
         setCelebrity();
+        updateScore();
     }
 
+    //Important function that sets both the celebrity image and button text
     public void setCelebrity() {
-        Log.i("TAG","setCelebrity");
-        //Code to parse image into ImageView
-        Bitmap celebImage;
-        int chosenCeleb = generateRandomNumber(names.size());
-        ImageDownloader imageTask = new ImageDownloader();
-        answer = names.get(chosenCeleb);
-        answerPosition = generateRandomNumber(4);
-        try{
-            celebImage = imageTask.execute(imgURLs.get(chosenCeleb)).get();
-            celebImageView.setImageBitmap(celebImage);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        //Game ending condition is checked first
+        if(previouslyDone.size() == names.size()){
+            gameFinished = true;
+            resetButton.setVisibility(View.VISIBLE);
+            buttonEnablerDisabler();
+        } else {
+            Log.i("TAG", "setCelebrity");
 
-        //Code for setting Button text
-        for(int i=0; i<=3; i++){
-            int id = getResources().getIdentifier("button"+i,"id",getPackageName());
-            button = findViewById(id);
-            int incorrectAnswerPosition;
-            if(i == answerPosition) {
-                button.setText(answer);
-            } else {
-                incorrectAnswerPosition = generateRandomNumber(names.size());
+            //Choosing celebrity at random
+            int chosenCeleb = generateRandomNumber(names.size());
 
-                while(incorrectAnswerPosition == chosenCeleb){
-                    incorrectAnswerPosition = generateRandomNumber(names.size());
-                }
-                button.setText(names.get(incorrectAnswerPosition));
+            while (previouslyDone.contains(chosenCeleb)) {
+                chosenCeleb = generateRandomNumber(names.size());
             }
 
+            answer = names.get(chosenCeleb);
+            answerPosition = generateRandomNumber(4);
 
+            //Code to parse image into ImageView
+            Bitmap celebImage;
+            ImageDownloader imageTask = new ImageDownloader();
+            try {
+                celebImage = imageTask.execute(imgURLs.get(chosenCeleb)).get();
+                celebImageView.setImageBitmap(celebImage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Code for setting Button text
+            for (int i = 0; i <= 3; i++) {
+                int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+                button = findViewById(id);
+                int incorrectAnswerPosition;
+                if (i == answerPosition) {
+                    button.setText(answer);
+                } else {
+                    incorrectAnswerPosition = generateRandomNumber(names.size());
+                    while (incorrectAnswerPosition != chosenCeleb) {
+                        button.setText(names.get(incorrectAnswerPosition));
+                        break;
+                    }
+                }
+            }
+            previouslyDone.add(chosenCeleb);
         }
-
     }
 
     //Background thread that downloads the image URL data and then converts it into a bitmap
     public class ImageDownloader extends AsyncTask<String,Void, Bitmap> {
-
         @Override
-
         protected Bitmap doInBackground(String... urls) {
             Log.i("TAG","Image Downloader");
                 try {
+
                     URL url = new URL(urls[0]);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.connect();
-
                     InputStream in = connection.getInputStream();
                     Bitmap myBitmap = BitmapFactory.decodeStream(in);
 
@@ -112,7 +151,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-
 
     //Background thread that downloads the html data and then converts it into a string
     public class DownloadTask extends AsyncTask<String,Void,String> {
@@ -145,13 +183,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         celebImageView = findViewById(R.id.celebImageView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        resetButton = findViewById(R.id.resetButton);
 
         String result = null;
         try {
@@ -163,7 +203,6 @@ public class MainActivity extends AppCompatActivity {
             Matcher m = p.matcher(splitResult[0]);
 
             while (m.find()){
-                //System.out.println(m.group(1));
                 imgURLs.add(m.group(1));
             }
 
@@ -178,9 +217,9 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e){
             e.printStackTrace();
         }
-        //System.out.println(names);
         Log.i("TAG","Initial phase complete");
         setCelebrity();
+        updateScore();
 
     }
 }
